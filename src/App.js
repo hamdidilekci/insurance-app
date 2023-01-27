c33ae4143eb30ce326145af7156aa192b52c8bc6
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2023-01-27 15:58:29 +0300

    create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MyLayout from './Components/MyLayout'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyLayout/>
+    </div>
+  );
+}
+
+export default App;
