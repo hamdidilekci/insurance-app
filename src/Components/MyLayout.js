3883fed03dba0297f68fd58b610d531dd1da5a77
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2023-01-29 23:47:57 +0300

    remove sider, add container to layout

--- src/Components/MyLayout.js
+++ src/Components/MyLayout.js
@@ -1,85 +1,36 @@
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import { Layout, Menu, theme } from 'antd';
+import { Layout, Menu } from 'antd';
 import React from 'react';
 import MyForm from './MyForm';
 
-const { Header, Content, Footer, Sider } = Layout;
+const { Header, Content, Footer } = Layout;
 
 const items1 = ['Anasayfa', 'Hakkımızda', 'İletişim'].map((key) => ({
     key,
     label: `${key}`,
 }));
 
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
-    const key = String(index + 1);
-    return {
-        key: `sub${key}`,
-        icon: React.createElement(icon),
-        label: `subnav ${key}`,
-        children: new Array(4).fill(null).map((_, j) => {
-            const subKey = index * 4 + j + 1;
-            return {
-                key: subKey,
-                label: `option${subKey}`,
-            };
-        }),
-    };
-});
-
 const MyLayout = () => {
-    const {
-        token: { colorBgContainer },
-    } = theme.useToken();
     return (
-        <Layout>
-            <Header className="header">
-                <div className="logo" />
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['Anasayfa']} items={items1} />
-            </Header>
-            <Content
-                style={{
-                    padding: '0 50px',
-                }}
-            >
-                <Layout
-                    style={{
-                        padding: '24px 0',
-                        background: colorBgContainer,
-                    }}
-                >
-                    <Sider
-                        style={{
-                            background: colorBgContainer,
-                        }}
-                        width={200}
-                    >
-                        <Menu
-                            mode="inline"
-                            defaultSelectedKeys={['1']}
-                            defaultOpenKeys={['sub1']}
-                            style={{
-                                height: '100%',
-                            }}
-                            items={items2}
-                        />
-                    </Sider>
-                    <Content
-                        style={{
-                           margin: '0' 
-                        }}
-                    >
-                        <MyForm/>
-                    </Content>
-                </Layout>
-            </Content>
-            <Footer
-                style={{
-                    textAlign: 'center',
-                }}
-            >
-                ©2023 Created by <a href="https://www.linkedin.com/in/dilekcihamdi/">Dilekçi</a>
-            </Footer>
-        </Layout>
+        <div className='image'>
+            <Layout className='container'>
+                <Header className="header">
+                    <Menu
+                        className='menu'
+                        theme='dark'
+                        mode="horizontal"
+                        defaultSelectedKeys={['Anasayfa']}
+                        items={items1}
+                    />
+                </Header>
+                <Content className='content'>
+                    <MyForm />
+                </Content>
+                <Footer className='footer'>
+                    ©2023 Created by <a href="https://www.linkedin.com/in/dilekcihamdi/">Dilekçi</a>
+                </Footer>
+            </Layout>
+        </div>
+        
     );
 };
 export default MyLayout;
