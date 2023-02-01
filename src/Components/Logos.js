3cb827742a656644ed790540cdad6ecd498513a3
dilekcihamdi-avena <98960357+hamdidilekci@users.noreply.github.com>
2023-02-02 02:00:33 +0300

    add insurance companies logos

---
+++ src/Components/Logos.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import acnturk from '../assets/acnturk.png';
+import ak from '../assets/aksigorta.jpg';
+import allianz from '../assets/allianz.jpg';
+import anadolu from '../assets/anadolusigorta.jpg';
+import ankara from '../assets/ankarasigorta.png';
+import bereket from '../assets/bereketsigorta.png';
+import bnpparibas from '../assets/bnpparabiascardif.jpg';
+import doga from '../assets/dogasigorta.jpg';
+import hdi from '../assets/HDI_SIGORTA_LOGO_-01-01.jpg'
+import eureko from '../assets/eureko.jpg';
+import generali from '../assets/generali.jpg';
+import groupama from '../assets/groupama.jpg';
+import hepiyi from '../assets/hepiyi.jpeg';
+import mapfree from '../assets/mapfresigorta.jpg';
+import neova from '../assets/neovasigorta.jpg';
+import quick from '../assets/quick.jpg';
+import ray from '../assets/raysigorta.jpg';
+import seker from '../assets/sekersigorta.jpg';
+import sompo from '../assets/somposigorta.jpg';
+import turkiye from '../assets/turkiyesigorta.jpg';
+import zurich from '../assets/zurichsigorta.jpg';
+
+export default function Logos() {
+  return (
+    <div>
+        <img src={acnturk} alt="acnturk" />
+        <img src={neova} alt="neova" />
+        <img src={quick} alt="quick" />
+        <img src={ray} alt="ray" />
+        <img src={seker} alt="seker" />
+        <img src={sompo} alt="sompo" />
+        <img src={turkiye} alt="turkiye" />
+        <img src={zurich} alt="zurich" />
+        <img src={ak} alt="ak" />
+        <img src={allianz} alt="allianz" />
+        <img src={anadolu} alt="anadolu" />
+        <img src={ankara} alt="ankara" />
+        <img src={bereket} alt="bereket" />
+        <img src={bnpparibas} alt="bnpparibas" />
+        <img src={doga} alt="doga" />
+        <img src={hdi} alt="hdi" />
+        <img src={eureko} alt="eureko" />
+        <img src={generali} alt="generali" />
+        <img src={groupama} alt="groupama" />
+        <img src={hepiyi} alt="hepiyi" />
+        <img src={mapfree} alt="mapfree" />
+    </div>
+  )
+}
